ade7e5ef8131fa2c7d3050f26f393ea457be1687
Nolberto Castellanos <nolberto0111@gmail.com>
2022-09-25 18:49:32 -0500

    Added the empty songs placeholder

--- app/src/main/java/com/betocrod/songrecords/designsystem/previewparameters/SampleSongProvider.kt
+++ app/src/main/java/com/betocrod/songrecords/designsystem/previewparameters/SampleSongProvider.kt
@@ -3,17 +3,21 @@ package com.betocrod.songrecords.designsystem.previewparameters
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.betocrod.songrecords.domain.models.Song
 
-class SampleSongProvider: PreviewParameterProvider<Song> {
+class SampleSongProvider : PreviewParameterProvider<Song> {
     override val values: Sequence<Song>
         get() {
             return getSongList().asSequence()
         }
 }
 
-class SampleSongListProvider: PreviewParameterProvider<List<Song>> {
+class SampleSongListProvider : PreviewParameterProvider<List<Song>> {
     override val values: Sequence<List<Song>>
-        get() = sequenceOf(getSongList())
+        get() = sequenceOf(
+            getSongList(),
+            emptyList()
+        )
 }
+
 private fun getSongList() = (1..10).map {
     Song(
         name = "Song name #$it",
--- app/src/main/java/com/betocrod/songrecords/features/home/widgets/SongListWidget.kt
+++ app/src/main/java/com/betocrod/songrecords/features/home/widgets/SongListWidget.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import com.betocrod.songrecords.R
 import com.betocrod.songrecords.designsystem.previewparameters.SampleSongListProvider
 import com.betocrod.songrecords.domain.models.Song
 import com.betocrod.songrecords.ui.theme.SongRecordsTheme
@@ -19,8 +23,21 @@ fun SongListWidget(
     songs: List<Song>,
     modifier: Modifier = Modifier
 ) {
+    if (songs.isNotEmpty()) {
+        NotEmptyListWidget(modifier, songs)
+    } else {
+        EmptyListPlaceHolder(modifier)
+    }
+}
+
+@Composable
+private fun NotEmptyListWidget(
+    modifier: Modifier,
+    songs: List<Song>
+) {
     LazyColumn(
-        modifier = modifier.background(MaterialTheme.colorScheme.background),
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background),
         contentPadding = PaddingValues(8.dp)
     ) {
         items(songs) {
@@ -33,12 +50,22 @@ fun SongListWidget(
     }
 }
 
+@Composable
+fun EmptyListPlaceHolder(modifier: Modifier) {
+    Box(modifier = modifier.background(MaterialTheme.colorScheme.background)) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = stringResource(R.string.feature_home_no_songs_placeholder)
+        )
+    }
+}
+
 @Preview("Song List Widget")
 @Composable
 fun PreviewSongListWidget(
     @PreviewParameter(SampleSongListProvider::class) songs: List<Song>
 ) {
     SongRecordsTheme {
-        SongListWidget(songs = songs)
+        SongListWidget(songs = songs, modifier = Modifier.fillMaxSize())
     }
 }
