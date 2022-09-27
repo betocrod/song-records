f2e3501e0442cc6b1c99526a4ae23208611ba2f6
Nolberto Castellanos <nolberto0111@gmail.com>
2022-09-26 20:57:22 -0500

    Added the RecordListWidget.kt

--- features/song/impl/src/main/java/com/betocrod/features/song/impl/widgets/previewparameters/SampleRecordProvider.kt
+++ features/song/impl/src/main/java/com/betocrod/features/song/impl/widgets/previewparameters/SampleRecordProvider.kt
@@ -3,9 +3,19 @@ package com.betocrod.features.song.impl.widgets.previewparameters
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.betocrod.features.song.impl.domain.models.Record
 
-class SampleRecordProvider: PreviewParameterProvider<Record> {
+class SampleRecordProvider : PreviewParameterProvider<Record> {
     override val values: Sequence<Record>
         get() = sequenceOf(
             Record("March 25, 2022", "Vocal warming")
         )
+}
+
+class SampleRecordListProvider : PreviewParameterProvider<List<Record>> {
+
+    override val values: Sequence<List<Record>>
+        get() = sequenceOf(
+            (0..15).map { Record("March $it, 2022", "Vocal warming $it") },
+            listOf()
+        )
+
 }
