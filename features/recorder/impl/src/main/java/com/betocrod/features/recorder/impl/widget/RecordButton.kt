bfd772b79e0adf6ef81d027c967f28ee160ec3b3
Nolberto Castellanos <nolberto0111@gmail.com>
2022-10-02 18:43:28 -0500

    Updated the record button style

--- features/recorder/impl/src/main/java/com/betocrod/features/recorder/impl/widget/RecordButton.kt
+++ features/recorder/impl/src/main/java/com/betocrod/features/recorder/impl/widget/RecordButton.kt
@@ -1,12 +1,15 @@
 package com.betocrod.features.recorder.impl.widget
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.betocrod.designsystem.DSDrawable
 import com.betocrod.designsystem.SongRecordsTheme
 import com.betocrod.features.recorder.impl.widget.previewparameters.SamplePreviewProvider
@@ -18,12 +21,14 @@ fun RecordButton(
     modifier: Modifier = Modifier
 ) {
     IconButton(
-        modifier = modifier,
+        modifier = modifier.size(64.dp),
         onClick = { onRecordClick() },
         content = {
             Icon(
+                modifier = Modifier.size(64.dp),
                 painter = painterResource(id = if (recording) DSDrawable.ic_stop_circle else DSDrawable.ic_record),
-                contentDescription = null
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.tertiary
             )
         }
     )
