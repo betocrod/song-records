aec705fe6d489ab83c1c63cd5b5bc3a99b034739
Nolberto Castellanos <nolberto0111@gmail.com>
2022-10-03 00:29:07 -0500

    Decouple record audio logic from the record feature

---
+++ domains/audios/api/src/main/java/com/betocrod/features/audios/api/usecases/RecordAudioUC.kt
@@ -0,0 +1,8 @@
+package com.betocrod.features.audios.api.usecases
+
+interface RecordAudioUC {
+
+    fun start()
+
+    fun stop()
+}
---
+++ domains/audios/impl/src/main/java/com/betocrod/features/audios/impl/usecase/RecordAudioUCImpl.kt
@@ -0,0 +1,63 @@
+package com.betocrod.features.audios.impl.usecase
+
+import android.content.Context
+import android.media.MediaRecorder
+import com.betocrod.features.audios.api.usecases.RecordAudioUC
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.inject.Inject
+
+class RecordAudioUCImpl @Inject constructor(
+    @ApplicationContext private val context: Context
+) : RecordAudioUC {
+
+    private var mediaRecorder: MediaRecorder? = null
+
+    override fun start() {
+        val path = getFilePath()
+        mediaRecorder = MediaRecorder(context).apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(path)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            prepare()
+            start()
+        }
+    }
+
+    private fun getFilePath(): String {
+        val dir = getRecordsDir()
+        val fileName = getFileName()
+        val file = File(dir, fileName)
+        file.createNewFile()
+        return file.absolutePath
+    }
+
+    override fun stop() {
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+        mediaRecorder = null
+    }
+
+    private fun getRecordsDir(): File {
+        val dir = File(context.filesDir, RECORD_DIR_NAME)
+        if (!dir.isDirectory) {
+            dir.mkdir()
+        }
+        return dir
+    }
+
+    private fun getFileName(): String {
+        val pattern = "dd-MM-yyyy - HH:mm:ss"
+        val dateTime = SimpleDateFormat(pattern, Locale.getDefault()).format(Date())
+        return "record_$dateTime.3gp"
+    }
+
+    companion object {
+        private const val RECORD_DIR_NAME = "records"
+    }
+}
